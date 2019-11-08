cf88949c39766c774238288c35a6a1d3dceadb81
rogierrr <rogierrr@hotmail.com>
2019-11-08 21:40:40 +0100

    Excer. 1, 6, & 18

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,18 @@
+// Write your JS here
+
+let hero = {} 
+
+console.log(hero)
+
+function rest() {
+    console.log(rest + "");
+}
+
+function pickUpItem(person, weapon) {
+    console.log(pickUpItem + "");
+}
+
+function equipWeapon() {
+    console.log(rest + "");  
+}
+
