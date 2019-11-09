de2ef908c230a81590a4eb84709c2ce63721fdd0
Rogier van Beers <rogierrr@hotmail.com>
2019-11-09 10:28:14 +0100

    Update YOUR_JAVASCRIPT_HERE.js

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,21 +1,29 @@
 // Write your JS here
 
-let hero = {} 
+// Hero Object:
+
+let hero = {name: "", heroic: (10>9) , inventory: [], health: 10, weapon:{ type: "", damage: 2},} 
 
 console.log(hero)
+  
+
+
+
+// Game logic functions:
 
 function rest() {
     console.log(rest + "");
 }
+
 
 function pickUpItem(person, weapon) {
     console.log(pickUpItem + "");
 }
 
 function equipWeapon() {
-    console.log(rest + "");  
+    console.log(equipWeapon + "");  
 }
 
 
-console.log("test");
+
 
